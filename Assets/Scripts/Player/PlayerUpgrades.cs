9824abc59aeaead5f247b28cf67c39cead34a468
ILostAccessToYcGun <ycgun32@gmail.com>
2025-03-22 16:21:53 -0400

    Revert "gonna try getting rid of the Invalid AABB inAABB"
    This reverts commit 771042591001ca866dfb63eeb213a4a5a6195185.

--- Assets/Scripts/Player/PlayerUpgrades.cs
+++ Assets/Scripts/Player/PlayerUpgrades.cs
@@ -28,7 +28,7 @@ public class PlayerUpgrades : MonoBehaviour
 
 
 
-    public void Mobility_onClick()
+    public void Mobility()
     {
         movement.M_walkSpeed = Mathf.Clamp(movement.M_walkSpeed + 1.8f, 18f, 36f);
         movement.M_sprintMultiplier = Mathf.Clamp(movement.M_sprintMultiplier + 0.16f, 1.6f, 3.2f);
@@ -37,7 +37,7 @@ public class PlayerUpgrades : MonoBehaviour
         movement.M_jumpSpeed = Mathf.Clamp(movement.M_jumpSpeed + 2f, 20f, 40f);
     }
 
-    public void Dash_onClick()
+    public void Dash()
     {
         movement.M_dashSpeed = Mathf.Clamp(movement.M_dashSpeed + 4.5f, 45f, 90f);
         movement.M_dashBetweenCooldown = Mathf.Clamp(movement.M_dashBetweenCooldown - 0.01f, 0.1f, 0.2f);
@@ -49,7 +49,7 @@ public class PlayerUpgrades : MonoBehaviour
             tempMaxDashHold = 0;
         }
     }
-    public void Boost_onClick()
+    public void Boost()
     {
         movement.M_maxBoost = Mathf.Clamp(movement.M_maxBoost + 10f, 100f, 200f);
         movement.M_boostGain = Mathf.Clamp(movement.M_boostGain + 0.5f, 5f, 10f);
@@ -62,7 +62,7 @@ public class PlayerUpgrades : MonoBehaviour
 
     #region _Gun_
 
-    public void Bullets_onClick()
+    public void Bullets()
     {
         ++tempBulletDamageHold;
         if (tempBulletDamageHold == 2)
@@ -74,7 +74,7 @@ public class PlayerUpgrades : MonoBehaviour
         gun.M_range = Mathf.Clamp(gun.M_range + 2f, 20f, 40f);
     }
 
-    public void Magazine_onClick()
+    public void Magazine()
     {
         ++tempMaxAmmoHold;
         if (tempMaxAmmoHold == 2)
@@ -93,7 +93,7 @@ public class PlayerUpgrades : MonoBehaviour
 
     #region _Battery_
 
-    public void Battery_onClick()
+    public void Battery()
     {
         battery.M_maxBattery = Mathf.Clamp(gun.M_reloadTime + 10f, 100f, 200f);
         battery.M_passiveLossRate = Mathf.Clamp(gun.M_reloadTime - 0.025f, 0.25f, 0.5f);
@@ -103,7 +103,7 @@ public class PlayerUpgrades : MonoBehaviour
 
     #region _Health_Bar_
 
-    public void Frame_onClick()
+    public void Frame()
     {
         healthBar.M_maxHealth = Mathf.Clamp(gun.M_reloadTime + 10f, 100f, 200f);
         healthBar.M_damageToEnergyLoss = Mathf.Clamp(gun.M_reloadTime - 0.005f, 0.05f, 0.1f);
@@ -136,9 +136,7 @@ public class PlayerUpgrades : MonoBehaviour
                     break;
             }
         }
-        //abandonConfirmButton.onClick.RemoveAllListeners();
-        //abandonConfirmButton.onClick.AddListener(this.ButtonConfirmAbandon_onClick);
-
+        
         target.onClick.RemoveAllListeners();
         bool isValidUpgrade = false;
         int choice = 0;
@@ -161,31 +159,31 @@ public class PlayerUpgrades : MonoBehaviour
         switch (choice)
         {
             case 0:
-                target.onClick.AddListener(this.Mobility_onClick);
+                target.onClick.AddListener(this.Mobility);
                 methodIndexBlacklist.Add(0);
                 break;
             case 1:
-                target.onClick.AddListener(this.Dash_onClick);
+                target.onClick.AddListener(this.Dash);
                 methodIndexBlacklist.Add(1);
                 break;
             case 2:
-                target.onClick.AddListener(this.Boost_onClick);
+                target.onClick.AddListener(this.Boost);
                 methodIndexBlacklist.Add(2);
                 break;
             case 3:
-                target.onClick.AddListener(this.Bullets_onClick);
+                target.onClick.AddListener(this.Bullets);
                 methodIndexBlacklist.Add(3);
                 break;
             case 4:
-                target.onClick.AddListener(this.Magazine_onClick);
+                target.onClick.AddListener(this.Magazine);
                 methodIndexBlacklist.Add(4);
                 break;
             case 5:
-                target.onClick.AddListener(this.Battery_onClick);
+                target.onClick.AddListener(this.Battery);
                 methodIndexBlacklist.Add(5);
                 break;
             case 6:
-                target.onClick.AddListener(this.Frame_onClick);
+                target.onClick.AddListener(this.Frame);
                 methodIndexBlacklist.Add(6);
                 break;
         }
